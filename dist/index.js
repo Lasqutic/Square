0fd15d540b01fbe9af7db03f49380b16b5f16683
Lasqutic <lasqutic@protonmail.com>
2025-04-25 11:32:15 +0300

    delete the /dist directory and add it to .gitignore

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const square_service_1 = require("./square.service");
-const square = new square_service_1.Square(5);
-console.log("Side:", square.getSideLength());
-console.log(square.getArea());
-console.log(square.getPerimeter());
